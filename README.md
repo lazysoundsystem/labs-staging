9fde2e81b383adba7936a42f2cf95ed9931c3707
Andy Footner <andyfootner@netscape.net>
2015-05-12 15:13:31 +0200

    horizontal version for testing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# labs-staging
-staging site for labs changes
+labs
+====
+
+ReliefWeb Labs Staging.
+*This is only for showing and testing changes to the main ReliefWeb Labs repo.*
+
+Github pages site for information about ReliefWeb Labs projects, to highlight
+other significant humanitarian community projects and to display innovation
+news related to the humanitarian community.
---
+++ js/scrolling.js
@@ -0,0 +1,16 @@
+// http://alijafarian.com/responsive-page-scrolling-with-jquery-and-bootstrap
+$(document).ready(function() {
+	// navigation click actions
+	$('.scroll-link').on('click', function(event){
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		scrollToID('#' + sectionID, 750);
+	});
+});
+// scroll function
+function scrollToID(id, speed){
+	var offSet = 50;
+	var targetOffset = $(id).offset().top - offSet;
+	var mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+}
